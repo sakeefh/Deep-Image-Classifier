8e3dd5899ece288a759c6745aa80a48a9dd1ccae
Sakeef Hassan <91638600+sakeefh@users.noreply.github.com>
2024-04-23 01:31:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras import datasets, layers, models
 
-
+# Load CIFAR-10 dataset
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
 training_images, testing_images = training_images /255, testing_images / 255
 
+# Define class names for CIFAR-10 dataset
 class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck',]
 
+# Visualize sample images
 for i in range(16):
     plt.subplot(4,4,i+1)
     plt.xticks([])
@@ -16,11 +18,21 @@ for i in range(16):
     plt.imshow(training_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[training_labels[i][0]])
 
-plt.show()
-
-training_images = training_images[:20000]
+# Reduce the size of the dataset for faster testing
+training_images = training_images[:20000] # first 20,000 training images
 training_labels = training_labels[:20000]
-testing_images = testing_images[:4000]
+testing_images = testing_images[:4000] # first 4,000 testing images
 testing_labels = testing_labels[:4000]
 
 model = models.load_model('image_classifier.keras')
+
+img = cv.imread('XYZ.jpg') # Insert your image file here
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+plt.imshow(img, cmap=plt.cm.binary)
+
+prediction = model.predict(np.array([img]) / 255)
+index = np.argmax(prediction)
+print(f'Prediction is {class_names[index]}')
+
+plt.show()
