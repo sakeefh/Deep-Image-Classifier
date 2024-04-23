b3e8f506919da53e43f3fb71bdab40b0b88965ca
Sakeef Hassan <91638600+sakeefh@users.noreply.github.com>
2024-04-23 01:07:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras import datasets, layers, models
 
+
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
 training_images, testing_images = training_images /255, testing_images / 255
 
@@ -22,25 +23,4 @@ training_labels = training_labels[:20000]
 testing_images = testing_images[:4000]
 testing_labels = testing_labels[:4000]
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
-
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
-
-loss, accuracy = model.evaluate(testing_images, testing_labels)
-print(f"Loss: {loss}")
-print(f"Accuracy: {accuracy}")
-
-model.save('image_classifier.h5')
-
-
-
+model = models.load_model('image_classifier.keras')
